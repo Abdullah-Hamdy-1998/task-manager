ecaaef3dc8aae2fd4a88b4cafdc660b6f554ef72
Abdullah Hamdy <abdullah.hamdi.1998@gmail.com>
2025-08-26 06:52:52 +0300

    feat(TaskPolicy): implement authorization methods for task management

--- app/Policies/TaskPolicy.php
+++ app/Policies/TaskPolicy.php
@@ -2,11 +2,78 @@
 
 namespace App\Policies;
 
+use App\Models\Task;
 use App\Models\User;
 
 class TaskPolicy
 {
-    public function create(User $user): bool
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user)
+    {
+        return $user->role->name === 'manager';
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, Task $task)
+    {
+        return $user->role->name === 'manager' || $user->id === $task->assignee_id;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        return $user->role->name === 'manager';
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user)
+    {
+        return $user->role->name === 'manager';
+    }
+
+    /**
+     * Determine whether the user can update the task status.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function updateStatus(User $user, Task $task)
+    {
+        return $user->role->name === 'manager' || $user->id === $task->assignee_id;
+    }
+
+    /**
+     * Determine whether the user can add a dependency to the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function addDependency(User $user)
     {
         return $user->role->name === 'manager';
     }
