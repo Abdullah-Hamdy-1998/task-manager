5d4a4a9bf76a9b27ae6e1fde985f649b3115aa4a
Abdullah Hamdy <abdullah.hamdi.1998@gmail.com>
2025-08-26 08:09:16 +0300

    feat: add UpdateTaskRequest for task update validation

---
+++ app/Http/Requests/UpdateTaskRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateTaskRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'title' => 'sometimes|string|max:255',
+            'description' => 'sometimes|string',
+            'assignee_id' => 'sometimes|integer|exists:users,id',
+            'due_date' => 'sometimes|date',
+        ];
+    }
+}
